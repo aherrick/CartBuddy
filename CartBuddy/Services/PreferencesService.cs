6a440f5c3211a2195d22ea492e2216d132dd185e
Andrew Herrick <aj@andrewherrick.com>
2026-03-10 14:20:53 -0400

    Use nameof for preference keys
    Replace hardcoded preference key constants with nameof(...) in property getters/setters to remove duplicated string literals and reduce typo risk. Simplify Theme setter and ApplyTheme to use the null-conditional operator (Application.Current?.UserAppTheme = ...) and convert ApplyTheme to an expression-bodied method. Removed the now-unused private key constants and cleaned up null checks.

--- CartBuddy/Services/PreferencesService.cs
+++ CartBuddy/Services/PreferencesService.cs
@@ -4,39 +4,31 @@ namespace CartBuddy.Services;
 
 public class PreferencesService(IConfiguration config)
 {
-    private const string ZipCodeKey = "zip_code";
-    private const string StoreIdKey = "store_id";
-    private const string StoreNameKey = "store_name";
-    private const string ThemeKey = "theme";
-
     public string ZipCode
     {
-        get => Preferences.Default.Get(ZipCodeKey, "");
-        set => Preferences.Default.Set(ZipCodeKey, value);
+        get => Preferences.Default.Get(nameof(ZipCode), "");
+        set => Preferences.Default.Set(nameof(ZipCode), value);
     }
 
     public string StoreId
     {
-        get => Preferences.Default.Get(StoreIdKey, "");
-        set => Preferences.Default.Set(StoreIdKey, value);
+        get => Preferences.Default.Get(nameof(StoreId), "");
+        set => Preferences.Default.Set(nameof(StoreId), value);
     }
 
     public string StoreName
     {
-        get => Preferences.Default.Get(StoreNameKey, "");
-        set => Preferences.Default.Set(StoreNameKey, value);
+        get => Preferences.Default.Get(nameof(StoreName), "");
+        set => Preferences.Default.Set(nameof(StoreName), value);
     }
 
     public AppTheme Theme
     {
-        get => (AppTheme)Preferences.Default.Get(ThemeKey, (int)AppTheme.Dark);
+        get => (AppTheme)Preferences.Default.Get(nameof(Theme), (int)AppTheme.Dark);
         set
         {
-            Preferences.Default.Set(ThemeKey, (int)value);
-            if (Application.Current is not null)
-            {
-                Application.Current.UserAppTheme = value;
-            }
+            Preferences.Default.Set(nameof(Theme), (int)value);
+            Application.Current?.UserAppTheme = value;
         }
     }
 
@@ -46,11 +38,5 @@ public class PreferencesService(IConfiguration config)
         !string.IsNullOrEmpty(config["AzureOpenAI:Endpoint"])
         && !string.IsNullOrEmpty(config["AzureOpenAI:Key"]);
 
-    public void ApplyTheme()
-    {
-        if (Application.Current is not null)
-        {
-            Application.Current.UserAppTheme = Theme;
-        }
-    }
+    public void ApplyTheme() => Application.Current?.UserAppTheme = Theme;
 }
